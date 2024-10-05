70171349f27780e4fdbe20abc6242dc04d4938d4
cloudtechstars <95733047+cloudtechstars@users.noreply.github.com>
2024-10-05 23:22:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,10 +19,10 @@ terraform {
 
 backend "remote" {
 		hostname = "app.terraform.io"
-		organization = "CloudQuickLabs"
+		organization = "Vijayalaxmi"
 
 		workspaces {
-			name = "AWSEKS"
+			name = "Vijaya"
 		}
 	}
 }
